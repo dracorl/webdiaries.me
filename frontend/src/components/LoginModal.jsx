bbe3deb77e00205c1289560e7c14f3b71752585a
dracorlll <dracorlll@gmail.com>
2024-05-11 14:45:56 +0300

    fix: different bg colors between modals changed

--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -6,7 +6,7 @@ const LoginModal = () => {
       {/* Open the modal using document.getElementById('ID').showModal() method */}
 
       <dialog id="loginModal" className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box bg-secondary">
+        <div className="modal-box">
           <form method="dialog">
             <h2 className="text-2xl text-center">Jump In</h2>
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
--- frontend/src/components/SignUpModal.jsx
+++ frontend/src/components/SignUpModal.jsx
@@ -4,7 +4,7 @@ const SignUpModal = () => {
   return (
     <>
       <dialog id="signUpModal" className="modal modal-bottom sm:modal-middle">
-        <div className="modal-box bg-secondary">
+        <div className="modal-box">
           <form method="dialog">
             <h2 className="text-2xl text-center">Sign Up</h2>
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
